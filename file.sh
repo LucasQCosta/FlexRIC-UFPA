fd937e31ad5374ca02887cc811d2e7db7c91e0e8
Lucas Queiroz Costa <107934478+LucasQCosta@users.noreply.github.com>
2023-05-15 15:18:28 -0300

    Update file.sh

--- file.sh
+++ file.sh
@@ -3,13 +3,16 @@
 sudo apt update
 
 # Installing Kitware APT Repository, recommended by FlexRIC team
-sudo apt install -y gpg wget
+
+sudo apt-get install gpg wget
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
-echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ focal main' | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
-sudo apt update
+echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ bionic main' | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
+sudo apt-get update
 sudo rm /usr/share/keyrings/kitware-archive-keyring.gpg
-sudo apt install -y kitware-archive-keyring
-echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ focal-rc main' | sudo tee -a /etc/apt/sources.list.d/kitware.list >/dev/null
+sudo apt-get install kitware-archive-keyring
+echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ bionic-rc main' | sudo tee -a /etc/apt/sources.list.d/kitware.list >/dev/null
+sudo apt-get update
+sudo apt-get install cmake
 
 # Some applications and libraries needed to install FlexRIC
 sudo apt update
@@ -19,14 +22,13 @@ sudo apt install -y libsctp-dev cmake-curses-gui libpcre2-dev python3-dev
 sudo apt install -y python3.8
 
 # PCRE2 is a dependency to Swig
-wget https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.42/pcre2-10.42.tar.bz2
-tar -xf pcre2-10.42.tar.bz2
-cd pcre2-10.42/
-./configure --prefix=/usr --docdir=/usr/share/doc/pcre2-10.42 --enable-unicode --enable-jit --enable-pcre2-16 --enable-pcre2-32 --enable-pcre2grep-libz --enable-pcre2grep-libbz2 --enable-pcre2test-libreadline --disable-static
-make
+wget https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.31/pcre2-10.31.tar.bz2
+tar -xf pcre2-10.31.tar.bz2
+cd pcre2-10.31/
+./configure --prefix=/usr --docdir=/usr/share/doc/pcre2-10.31 --enable-unicode --enable-jit --enable-pcre2-16 --enable-pcre2-32 --enable-pcre2grep-libz --enable-pcre2grep-libbz2 --enable-pcre2test-libreadline --disable-static
 sudo make install
 cd ..
-sudo rm -r pcre2-10.42 pcre2-10.42.tar.bz2 
+sudo rm -r pcre2-10.31 pcre2-10.31.tar.bz2 
 
 # Swig is a dependency to FlexRIC
 git clone https://github.com/swig/swig.git
